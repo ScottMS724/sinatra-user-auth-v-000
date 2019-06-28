59a982ad4cd45dbbd06d2a130f38bff08dd90fe4
Scott Silverman <scottms724@gmail.com>
2019-06-28 20:15:45 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,12 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
   
-  def up
+  def change 
     create_table :users do |t| 
       t.string :name 
       t.string :email 
       t.string :password 
     end 
-  end 
-  
-  def down 
-    drop_table :users 
   end 
   
   
